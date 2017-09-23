ffa8447ae5fb2295e62c0de85e1e7524be7d7796
Trần Anh Sơn <sontrananh96@gmail.com>
2017-09-23 23:59:31 +0700

    Animation LinearFilter
    Làm đc nhưng đọc ko hiểu lắm. Tính toàn ghê quá

--- JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/NTD/NTDSearchFilterFragment.java
+++ JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/NTD/NTDSearchFilterFragment.java
@@ -7,6 +7,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import com.spkt.nguyenducnguu.jobstore.Adaper.RecyclerViewAdapter;
 import com.spkt.nguyenducnguu.jobstore.R;
@@ -15,8 +19,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NTDSearchFilterFragment extends Fragment {
-
+    private static final int HIDE_THRESHOLD = 20;
+    private int scrolledDistance = 0;
+    private boolean controlsVisible = true;
+    LinearLayout linearLayout;
     RecyclerView mRecyclerView;
+    ScrollView scrollView;
     RecyclerViewAdapter mRcvAdapter;
     List<String> data;
     @Override
@@ -29,18 +37,62 @@ public class NTDSearchFilterFragment extends Fragment {
         FontManager.markAsIconContainer(rootView.findViewById(R.id.Main), iconFont);*/
 
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.rv_list_uv);
+
+        linearLayout = (LinearLayout) rootView.findViewById(R.id.ln_filter);
+        addOnScrolled();
+        addData();
+        setmRecyclerView();
+
+        return rootView;
+    }
+    private void addOnScrolled(){
+
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (scrolledDistance > HIDE_THRESHOLD && controlsVisible) {
+                    hideViews();
+                    controlsVisible = false;
+                    scrolledDistance = 0;
+                } else if (scrolledDistance < -HIDE_THRESHOLD && !controlsVisible) {
+                    showViews();
+                    controlsVisible = true;
+                    scrolledDistance = 0;
+                }
+
+                if((controlsVisible && dy>0) || (!controlsVisible && dy<0)) {
+                    scrolledDistance += dy;
+                }
+            }
+        });
+
+    }
+    private void hideViews() {
+        linearLayout.animate().translationY(-linearLayout.getHeight()).setInterpolator(new AccelerateInterpolator(2));
+    }
+
+    private void showViews() {
+        linearLayout.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2));
+    }
+    private void addData() {
         data = new ArrayList<>();
 
-        data.add("Nguyễn Minh Hưng");
+        data.add("Sang Sang Nguyễn");
         data.add("Hoàng Minh Lợi");
         data.add("Nguyễn Duy Bảo");
         data.add("Nguyễn Ngọc Doanh");
         data.add("Nguyễn Phạm Thế Hà");
         data.add("Trần Anh Đức");
         data.add("Trần Minh Hải");
+        data.add("Trần Minh Hải");
+        data.add("Trần Minh Hải");
+        data.add("Trần Minh Hải");
 
         mRcvAdapter = new RecyclerViewAdapter(data);
-
+    }
+    private void setmRecyclerView(){
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
 
@@ -48,7 +100,5 @@ public class NTDSearchFilterFragment extends Fragment {
         // ta sẽ set setHasFixedSize bằng True để tăng performance
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setAdapter(mRcvAdapter);
-
-        return rootView;
     }
 }
