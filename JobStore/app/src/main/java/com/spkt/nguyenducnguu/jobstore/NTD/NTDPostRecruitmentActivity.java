a080364e8f753eba8d0159ac5ce3986dbe65b08a
Trần Anh Sơn <sontrananh96@gmail.com>
2017-09-23 09:57:02 +0700

    Giao diện đăng tin NTD
    OK

--- JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/NTD/NTDPostRecruitmentActivity.java
+++ JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/NTD/NTDPostRecruitmentActivity.java
@@ -7,13 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import com.spkt.nguyenducnguu.jobstore.FontManager.FontManager;
 import com.spkt.nguyenducnguu.jobstore.R;
 
-public class NTDPostRecruitment2Activity extends AppCompatActivity {
+public class NTDPostRecruitmentActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ntd_post_recruitment2);
+        setContentView(R.layout.activity_ntd_post_recruitment);
 
         //Method để sử dụng font awesome trong fragment
         Typeface iconFont = FontManager.getTypeface(getApplicationContext(), FontManager.FONTAWESOME);
--- JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/NTD/NTDPostRecruitmentFragment.java
+++ JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/NTD/NTDPostRecruitmentFragment.java
@@ -1,19 +1,31 @@
 package com.spkt.nguyenducnguu.jobstore.NTD;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.spkt.nguyenducnguu.jobstore.R;
 
 public class NTDPostRecruitmentFragment extends Fragment {
+    Button btn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.ntd_post_recruitment_fragment, container, false);
 
+        btn = (Button) rootView.findViewById(R.id.buttonNext);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getContext() , NTDPostRecruitmentActivity.class);
+                startActivity(myIntent);
+            }
+        });
         return rootView;
     }
 
