987df40ae43d3696874c60bb7f9c3a4d938a7e0d
Trần Anh Sơn <sontrananh96@gmail.com>
2017-09-29 14:15:42 +0700

    Up
    SonTranAnh

---
+++ JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/NTD/NTDChangeProfile.java
@@ -0,0 +1,15 @@
+package com.spkt.nguyenducnguu.jobstore.NTD;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.spkt.nguyenducnguu.jobstore.R;
+
+public class NTDChangeProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ntd_change_profile);
+    }
+}
--- JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/NTD/NTDRecordManagementFragment.java
+++ JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/NTD/NTDRecordManagementFragment.java
@@ -1,11 +1,14 @@
 package com.spkt.nguyenducnguu.jobstore.NTD;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.spkt.nguyenducnguu.jobstore.FontManager.FontManager;
@@ -17,24 +20,42 @@ import com.spkt.nguyenducnguu.jobstore.R;
 
 public class NTDRecordManagementFragment extends Fragment {
     ImageView img_CoverPhoto;
-    TextView txt_icon1,txt_icon2,txt_icon3,txt_icon4,txt_icon5;
+    Button btn_UpCoverPhoto;
+    LinearLayout ln_changeProfile;
+    TextView txt_icon1,txt_icon2,txt_icon3,txt_icon4,txt_icon5, txt_icon6, txt_icon7;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.ntd_record_management_fragment, container, false);
 
         addView(rootView);
+        addEvent();
         setIcon();
         return rootView;
     }
 
     private void addView(View rootView){
         img_CoverPhoto = (ImageView) rootView.findViewById(R.id.img_CoverPhoto);
+        btn_UpCoverPhoto = (Button) rootView.findViewById(R.id.btn_UpCoverPhoto);
+        ln_changeProfile = (LinearLayout) rootView.findViewById(R.id.ln_changeProfile);
         txt_icon1 = (TextView) rootView.findViewById(R.id.txt_icon1);
         txt_icon2 = (TextView) rootView.findViewById(R.id.txt_icon2);
         txt_icon3 = (TextView) rootView.findViewById(R.id.txt_icon3);
         txt_icon4 = (TextView) rootView.findViewById(R.id.txt_icon4);
         txt_icon5 = (TextView) rootView.findViewById(R.id.txt_icon5);
+        txt_icon6 = (TextView) rootView.findViewById(R.id.txt_icon6);
+        txt_icon7 = (TextView) rootView.findViewById(R.id.txt_icon7);
+
+    }
+    private void addEvent()
+    {
+        ln_changeProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myItent = new Intent(getActivity(), NTDChangeProfile.class);
+                startActivity(myItent);
+            }
+        });
     }
     private void setIcon(){
         txt_icon1.setTypeface(FontManager.getTypeface(getActivity(),FontManager.FONTAWESOME));
@@ -42,5 +63,8 @@ public class NTDRecordManagementFragment extends Fragment {
         txt_icon3.setTypeface(FontManager.getTypeface(getActivity(),FontManager.FONTAWESOME));
         txt_icon4.setTypeface(FontManager.getTypeface(getActivity(),FontManager.FONTAWESOME));
         txt_icon5.setTypeface(FontManager.getTypeface(getActivity(),FontManager.FONTAWESOME));
+        txt_icon6.setTypeface(FontManager.getTypeface(getActivity(),FontManager.FONTAWESOME));
+        txt_icon7.setTypeface(FontManager.getTypeface(getActivity(),FontManager.FONTAWESOME));
+        btn_UpCoverPhoto.setTypeface(FontManager.getTypeface(getActivity(),FontManager.FONTAWESOME));
     }
 }
