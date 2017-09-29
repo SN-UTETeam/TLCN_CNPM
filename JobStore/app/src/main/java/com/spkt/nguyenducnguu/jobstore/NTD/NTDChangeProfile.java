77b68e6fde201f6066b54876189cbf9192c0e583
Trần Anh Sơn <sontrananh96@gmail.com>
2017-09-29 16:31:36 +0700

    up 1

--- JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/NTD/NTDChangeProfile.java
+++ JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/NTD/NTDChangeProfile.java
@@ -2,6 +2,8 @@ package com.spkt.nguyenducnguu.jobstore.NTD;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.spkt.nguyenducnguu.jobstore.FontManager.FontManager;
@@ -9,16 +11,18 @@ import com.spkt.nguyenducnguu.jobstore.R;
 
 public class NTDChangeProfile extends AppCompatActivity {
     TextView txt_icon1, txt_icon2, txt_icon3, txt_icon4, txt_icon5, txt_icon6, txt_icon7;
+    ImageView imgv_Back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ntd_change_profile);
 
         addView();
-
+        addEvent();
         setIcon();
     }
     private void addView(){
+        imgv_Back = (ImageView) findViewById(R.id.imgv_Back);
         txt_icon1 = (TextView) findViewById(R.id.txt_icon1);
         txt_icon2 = (TextView) findViewById(R.id.txt_icon2);
         txt_icon3 = (TextView) findViewById(R.id.txt_icon3);
@@ -26,6 +30,14 @@ public class NTDChangeProfile extends AppCompatActivity {
         txt_icon5 = (TextView) findViewById(R.id.txt_icon5);
         txt_icon6 = (TextView) findViewById(R.id.txt_icon6);
         txt_icon7 = (TextView) findViewById(R.id.txt_icon7);
+    }
+    private void addEvent(){
+        imgv_Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
     private void setIcon(){
         txt_icon1.setTypeface(FontManager.getTypeface(getApplicationContext(),FontManager.FONTAWESOME));
