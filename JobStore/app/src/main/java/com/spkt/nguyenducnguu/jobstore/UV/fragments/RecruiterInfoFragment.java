168febd650895ec382db2b43dc1b2b431db01b58
Trần Anh Sơn <sontrananh96@gmail.com>
2017-11-16 15:33:41 +0700

    abc

--- JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/UV/fragments/RecruiterInfoFragment.java
+++ JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/UV/fragments/RecruiterInfoFragment.java
@@ -1,8 +1,7 @@
 package com.spkt.nguyenducnguu.jobstore.UV.fragments;
 
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/UV/fragments/RecruiterWorkInfoFragment.java
+++ JobStore/app/src/main/java/com/spkt/nguyenducnguu/jobstore/UV/fragments/RecruiterWorkInfoFragment.java
@@ -1,18 +1,14 @@
 package com.spkt.nguyenducnguu.jobstore.UV.fragments;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.spkt.nguyenducnguu.jobstore.Const.Node;
